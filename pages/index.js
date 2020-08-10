ce042274bda87328eebf7d7b240c46a75dcd33ae
Elvis-rugamba <55624349+Elvis-rugamba@users.noreply.github.com>
2020-08-10 04:36:01 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,22 +11,22 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello, I&apos;m <a href="https://nextjs.org">Elvis Rugamba!</a>
+          Hello, I&apos;m <a href="https://nextjs.org">Elvis Rugamba</a>
         </h1>
 
         <p className={styles.description}>
-          I&apos;m fullstack engineer{' '}
+          I&apos;m fullstack engineer skilled in{' '}
           <code className={styles.code}>Node.js</code> and <code className={styles.code}>React.js</code>
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
+            <h3>About &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
+            <h3>Portfolio &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
@@ -34,7 +34,7 @@ export default function Home() {
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
+            <h3>Blogs &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
@@ -42,7 +42,7 @@ export default function Home() {
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Contact &rarr;</h3>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
